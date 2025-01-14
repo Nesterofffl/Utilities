0d4d4998a26ad8d393f3902751f3a1ba6e9ad7d6
nesterofffl <nestnik2017@Gmail.com>
2025-01-15 00:09:28 +1100

    1

---
+++ src/main/java/com/nesterofffl/utilities/UtilitiesClient.java
@@ -0,0 +1,10 @@
+package com.nesterofffl.utilities;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UtilitiesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
